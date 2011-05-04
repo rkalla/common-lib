127d4375960c108b5b1488f9ac5e3b4272c5ff42
Riyad Kalla <software@thebuzzmedia.com>
2011-05-04 11:47:24 -0700

    Finished ArrayUtils impl and test cases.
    Added build.xml script.

--- src/main/java/com/thebuzzmedia/common/util/ArrayUtils.java
+++ src/main/java/com/thebuzzmedia/common/util/ArrayUtils.java
@@ -79,20 +79,15 @@ public class ArrayUtils {
 					+ "] must be < array.length [" + array.length + "]");
 
 		// pre-define once
-		int j = 0;
+		int j;
 
-		// Loop until we get within values.length units before the end
+		// Loop until we cannot match values anymore
 		for (int end = (index + length - values.length); index <= end; index++) {
 			// Increment j as long as we can match values in-order
 			for (j = 0; j < values.length && values[j] == array[index + j]; j++)
 				;
 
-			/*
-			 * j will only get incremented to values.length if all the values
-			 * were matched, in-order, in which case 'index' is what we return.
-			 * Otherwise skip ahead by the number of matched values and start
-			 * trying to match again.
-			 */
+			// If j is values.length, we matched all.
 			if (j == values.length)
 				return index;
 			else
@@ -136,7 +131,7 @@ public class ArrayUtils {
 					+ "] must be < array.length [" + array.length + "]");
 
 		// pre-define once
-		int j = 0;
+		int j;
 
 		for (int end = (index + length); index < end; index++) {
 			byte b = array[index];
@@ -155,16 +150,141 @@ public class ArrayUtils {
 	 * REVERSE byte[] array, byte value
 	 * ========================================================================
 	 */
+	public static int lastIndexOf(byte[] array, byte value)
+			throws IllegalArgumentException {
+		if (array == null)
+			throw new IllegalArgumentException("array cannot be null");
+
+		return lastIndexOf(0, array.length, array, value);
+	}
+
+	public static int lastIndexOf(int index, byte[] array, byte value)
+			throws IllegalArgumentException {
+		if (array == null)
+			throw new IllegalArgumentException("array cannot be null");
+
+		return indexOf(index, array.length - index, array, value);
+	}
+
+	public static int lastIndexOf(int index, int length, byte[] array,
+			byte value) throws IllegalArgumentException {
+		if (array == null)
+			throw new IllegalArgumentException("array cannot be null");
+		if (index < 0 || length < 0 || (index + length) > array.length)
+			throw new IllegalArgumentException("index [" + index
+					+ "] must be >= 0, length [" + length
+					+ "] must be >= 0 and (index+length) [" + (index + length)
+					+ "] must be < array.length [" + array.length + "]");
+
+		for (int i = (index + length - 1); i >= index; i--) {
+			if (array[i] == value)
+				return i;
+		}
+
+		return INVALID_INDEX;
+	}
 
 	/*
 	 * ========================================================================
 	 * REVERSE byte[] array, byte[] values
 	 * ========================================================================
 	 */
+	public static int lastIndexOf(byte[] array, byte[] values)
+			throws IllegalArgumentException {
+		if (array == null)
+			throw new IllegalArgumentException("array cannot be null");
+
+		return lastIndexOf(0, array.length, array, values);
+	}
+
+	public static int lastIndexOf(int index, byte[] array, byte[] values)
+			throws IllegalArgumentException {
+		if (array == null)
+			throw new IllegalArgumentException("array cannot be null");
+
+		return lastIndexOf(index, array.length - index, array, values);
+	}
+
+	public static int lastIndexOf(int index, int length, byte[] array,
+			byte[] values) throws IllegalArgumentException {
+		if (array == null)
+			throw new IllegalArgumentException("array cannot be null");
+		if (values == null)
+			throw new IllegalArgumentException("values cannot be null");
+		if (length < values.length)
+			throw new IllegalArgumentException("length [" + length
+					+ "] must be >= values.length [" + values.length + "]");
+		if (index < 0 || length < 0 || (index + length) > array.length)
+			throw new IllegalArgumentException("index [" + index
+					+ "] must be >= 0, length [" + length
+					+ "] must be >= 0 and (index+length) [" + (index + length)
+					+ "] must be < array.length [" + array.length + "]");
+
+		// pre-define once
+		int j;
+
+		// Loop until it isn't possible for us to match values
+		for (int i = (index + length - 1), endIndex = (index + values.length - 1); i >= endIndex; i--) {
+			// Increment j for as many matches as we find
+			for (j = 0; j < values.length
+					&& array[i - j] == values[values.length - 1 - j]; j++)
+				;
+
+			// If we incremented j all the way, we matched all of values
+			if (j == values.length)
+				return i - j + 1;
+			else
+				i -= j;
+		}
+
+		return INVALID_INDEX;
+	}
 
 	/*
 	 * ========================================================================
 	 * REVERSE byte[] array, ANY byte[] values
 	 * ========================================================================
 	 */
+	public static int lastIndexOfAny(byte[] array, byte[] values)
+			throws IllegalArgumentException {
+		if (array == null)
+			throw new IllegalArgumentException("array cannot be null");
+
+		return lastIndexOfAny(0, array.length, array, values);
+	}
+
+	public static int lastIndexOfAny(int index, byte[] array, byte[] values)
+			throws IllegalArgumentException {
+		if (array == null)
+			throw new IllegalArgumentException("array cannot be null");
+
+		return lastIndexOfAny(index, array.length - index, array, values);
+	}
+
+	public static int lastIndexOfAny(int index, int length, byte[] array,
+			byte[] values) throws IllegalArgumentException {
+		if (array == null)
+			throw new IllegalArgumentException("array cannot be null");
+		if (values == null)
+			throw new IllegalArgumentException("values cannot be null");
+		if (index < 0 || length < 0 || (index + length) > array.length)
+			throw new IllegalArgumentException("index [" + index
+					+ "] must be >= 0, length [" + length
+					+ "] must be >= 0 and (index+length) [" + (index + length)
+					+ "] must be < array.length [" + array.length + "]");
+
+		// pre-define once
+		int j;
+
+		for (int i = (index + length - 1); i >= index; i--) {
+			byte b = array[i];
+
+			for (j = 0; j < values.length; j++) {
+				if (b == values[j])
+					return i;
+			}
+		}
+
+		return INVALID_INDEX;
+	}
 }
--- src/test/java/com/thebuzzmedia/common/util/ArrayUtilsTest.java
+++ src/test/java/com/thebuzzmedia/common/util/ArrayUtilsTest.java
@@ -295,4 +295,285 @@ public class ArrayUtilsTest {
 		assertEquals(-1, ArrayUtils.indexOfAny(10, 1, ARRAY, VALUE_0));
 		assertEquals(10, ArrayUtils.indexOfAny(10, 1, ARRAY, VALUE_5));
 	}
+	
+	/*
+	 * ========================================================================
+	 * REVERSE byte[] array, byte value
+	 * ========================================================================
+	 */
+	@Test
+	public void testLastIndexOfBaB() {
+		try {
+			ArrayUtils.lastIndexOf(null, (byte) 0);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		assertEquals(0, ArrayUtils.lastIndexOf(ARRAY, (byte) -5));
+		assertEquals(5, ArrayUtils.lastIndexOf(ARRAY, (byte) 0));
+		assertEquals(10, ArrayUtils.lastIndexOf(ARRAY, (byte) 5));
+	}
+
+	@Test
+	public void testLastIndexOfIBaB() {
+		try {
+			ArrayUtils.lastIndexOf(0, null, (byte) 0);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		try {
+			ArrayUtils.lastIndexOf(-1, ARRAY, (byte) 0);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		assertEquals(0, ArrayUtils.lastIndexOf(0, ARRAY, (byte) -5));
+		assertEquals(5, ArrayUtils.lastIndexOf(0, ARRAY, (byte) 0));
+		assertEquals(10, ArrayUtils.lastIndexOf(0, ARRAY, (byte) 5));
+
+		assertEquals(-1, ArrayUtils.lastIndexOf(5, ARRAY, (byte) -5));
+		assertEquals(5, ArrayUtils.lastIndexOf(5, ARRAY, (byte) 0));
+		assertEquals(10, ArrayUtils.lastIndexOf(5, ARRAY, (byte) 5));
+
+		assertEquals(-1, ArrayUtils.lastIndexOf(10, ARRAY, (byte) -5));
+		assertEquals(-1, ArrayUtils.lastIndexOf(10, ARRAY, (byte) 0));
+		assertEquals(10, ArrayUtils.lastIndexOf(10, ARRAY, (byte) 5));
+	}
+
+	@Test
+	public void testLastIndexOfIIBaB() {
+		try {
+			ArrayUtils.lastIndexOf(0, 1, null, (byte) 0);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		try {
+			ArrayUtils.lastIndexOf(-1, 1, ARRAY, (byte) 0);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		try {
+			ArrayUtils.lastIndexOf(0, -1, ARRAY, (byte) 0);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		try {
+			ArrayUtils.lastIndexOf(0, ARRAY.length * 2, ARRAY, (byte) 0);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		assertEquals(0, ArrayUtils.lastIndexOf(0, ARRAY.length, ARRAY, (byte) -5));
+		assertEquals(5, ArrayUtils.lastIndexOf(0, ARRAY.length, ARRAY, (byte) 0));
+		assertEquals(10, ArrayUtils.lastIndexOf(0, ARRAY.length, ARRAY, (byte) 5));
+
+		assertEquals(-1,
+				ArrayUtils.lastIndexOf(5, ARRAY.length - 5, ARRAY, (byte) -5));
+		assertEquals(5,
+				ArrayUtils.lastIndexOf(5, ARRAY.length - 5, ARRAY, (byte) 0));
+		assertEquals(10,
+				ArrayUtils.lastIndexOf(5, ARRAY.length - 5, ARRAY, (byte) 5));
+
+		assertEquals(-1, ArrayUtils.lastIndexOf(10, 1, ARRAY, (byte) -5));
+		assertEquals(-1, ArrayUtils.lastIndexOf(10, 1, ARRAY, (byte) 0));
+		assertEquals(10, ArrayUtils.lastIndexOf(10, 1, ARRAY, (byte) 5));
+	}
+	
+	/*
+	 * ========================================================================
+	 * REVERSE byte[] array, byte[] values
+	 * ========================================================================
+	 */
+	@Test
+	public void testLastIndexOfBaBa() {
+		try {
+			ArrayUtils.lastIndexOf(null, VALUES_ZERO);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		assertEquals(0, ArrayUtils.lastIndexOf(ARRAY, VALUES_NEG));
+		assertEquals(5, ArrayUtils.lastIndexOf(ARRAY, VALUES_ZERO));
+		assertEquals(6, ArrayUtils.lastIndexOf(ARRAY, VALUES_POS));
+	}
+
+	@Test
+	public void testLastIndexOfIBaBa() {
+		try {
+			ArrayUtils.lastIndexOf(0, null, VALUES_ZERO);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		try {
+			ArrayUtils.lastIndexOf(-1, ARRAY, VALUES_ZERO);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		assertEquals(0, ArrayUtils.lastIndexOf(0, ARRAY, VALUES_NEG));
+		assertEquals(5, ArrayUtils.lastIndexOf(0, ARRAY, VALUES_ZERO));
+		assertEquals(6, ArrayUtils.lastIndexOf(0, ARRAY, VALUES_POS));
+
+		assertEquals(-1, ArrayUtils.lastIndexOf(5, ARRAY, VALUES_NEG));
+		assertEquals(5, ArrayUtils.lastIndexOf(5, ARRAY, VALUES_ZERO));
+		assertEquals(6, ArrayUtils.lastIndexOf(6, ARRAY, VALUES_POS));
+
+		assertEquals(-1, ArrayUtils.lastIndexOf(6, ARRAY, VALUES_NEG));
+		assertEquals(-1, ArrayUtils.lastIndexOf(10, ARRAY, VALUES_ZERO));
+	}
+
+	@Test
+	public void testLastIndexOfIIBaBa() {
+		try {
+			ArrayUtils.lastIndexOf(0, 1, null, VALUES_ZERO);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		try {
+			ArrayUtils.lastIndexOf(-1, 1, ARRAY, VALUES_ZERO);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		try {
+			ArrayUtils.lastIndexOf(0, -1, ARRAY, VALUES_ZERO);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		try {
+			ArrayUtils.lastIndexOf(0, ARRAY.length * 2, ARRAY, VALUES_ZERO);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		assertEquals(0, ArrayUtils.lastIndexOf(0, ARRAY.length, ARRAY, VALUES_NEG));
+		assertEquals(5, ArrayUtils.lastIndexOf(0, ARRAY.length, ARRAY, VALUES_ZERO));
+		assertEquals(6, ArrayUtils.lastIndexOf(0, ARRAY.length, ARRAY, VALUES_POS));
+
+		assertEquals(-1,
+				ArrayUtils.lastIndexOf(5, ARRAY.length - 5, ARRAY, VALUES_NEG));
+		assertEquals(5,
+				ArrayUtils.lastIndexOf(5, ARRAY.length - 5, ARRAY, VALUES_ZERO));
+		assertEquals(6,
+				ArrayUtils.lastIndexOf(5, ARRAY.length - 5, ARRAY, VALUES_POS));
+
+		assertEquals(-1, ArrayUtils.lastIndexOf(6, 5, ARRAY, VALUES_NEG));
+		assertEquals(5, ArrayUtils.lastIndexOf(5, 1, ARRAY, VALUES_ZERO));
+		assertEquals(-1, ArrayUtils.lastIndexOf(2, 5, ARRAY, VALUES_POS));
+	}
+	
+	/*
+	 * ========================================================================
+	 * REVERSE byte[] array, ANY byte[] values
+	 * ========================================================================
+	 */
+	@Test
+	public void testLastIndexOfAnyBaBa() {
+		try {
+			ArrayUtils.lastIndexOfAny(null, VALUE_0);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		assertEquals(0, ArrayUtils.lastIndexOfAny(ARRAY, VALUE_N5));
+		assertEquals(5, ArrayUtils.lastIndexOfAny(ARRAY, VALUE_0));
+		assertEquals(10, ArrayUtils.lastIndexOfAny(ARRAY, VALUE_5));
+	}
+
+	@Test
+	public void testLastIndexOfAnyIBaBa() {
+		try {
+			ArrayUtils.lastIndexOfAny(0, null, VALUE_0);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		try {
+			ArrayUtils.lastIndexOfAny(-1, ARRAY, VALUE_0);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		assertEquals(0, ArrayUtils.lastIndexOfAny(0, ARRAY, VALUE_N5));
+		assertEquals(5, ArrayUtils.lastIndexOfAny(0, ARRAY, VALUE_0));
+		assertEquals(10, ArrayUtils.lastIndexOfAny(0, ARRAY, VALUE_5));
+
+		assertEquals(-1, ArrayUtils.lastIndexOfAny(5, ARRAY, VALUE_N5));
+		assertEquals(5, ArrayUtils.lastIndexOfAny(5, ARRAY, VALUE_0));
+		assertEquals(10, ArrayUtils.lastIndexOfAny(5, ARRAY, VALUE_5));
+
+		assertEquals(-1, ArrayUtils.lastIndexOfAny(10, ARRAY, VALUE_N5));
+		assertEquals(-1, ArrayUtils.lastIndexOfAny(10, ARRAY, VALUE_0));
+		assertEquals(10, ArrayUtils.lastIndexOfAny(10, ARRAY, VALUE_5));
+	}
+
+	@Test
+	public void testLastIndexOfAnyIIBaBa() {
+		try {
+			ArrayUtils.lastIndexOfAny(0, 1, null, VALUE_0);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		try {
+			ArrayUtils.lastIndexOfAny(-1, 1, ARRAY, VALUE_0);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		try {
+			ArrayUtils.lastIndexOfAny(0, -1, ARRAY, VALUE_0);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		try {
+			ArrayUtils.lastIndexOfAny(0, ARRAY.length * 2, ARRAY, VALUE_0);
+			assertTrue(false); // shouldn't get here
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		assertEquals(0, ArrayUtils.lastIndexOfAny(0, ARRAY.length, ARRAY, VALUE_N5));
+		assertEquals(5, ArrayUtils.lastIndexOfAny(0, ARRAY.length, ARRAY, VALUE_0));
+		assertEquals(10, ArrayUtils.lastIndexOfAny(0, ARRAY.length, ARRAY, VALUE_5));
+
+		assertEquals(-1,
+				ArrayUtils.lastIndexOfAny(5, ARRAY.length - 5, ARRAY, VALUE_N5));
+		assertEquals(5,
+				ArrayUtils.lastIndexOfAny(5, ARRAY.length - 5, ARRAY, VALUE_0));
+		assertEquals(10,
+				ArrayUtils.lastIndexOfAny(5, ARRAY.length - 5, ARRAY, VALUE_5));
+
+		assertEquals(-1, ArrayUtils.lastIndexOfAny(10, 1, ARRAY, VALUE_N5));
+		assertEquals(-1, ArrayUtils.lastIndexOfAny(10, 1, ARRAY, VALUE_0));
+		assertEquals(10, ArrayUtils.lastIndexOfAny(10, 1, ARRAY, VALUE_5));
+	}
 }
