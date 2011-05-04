318622bb075156c029b9ccafa2154191b5dd0908
Riyad Kalla <software@thebuzzmedia.com>
2011-05-04 15:42:56 -0700

    Updated README.
    Added license snippet to the top of all source files.

--- src/test/java/com/thebuzzmedia/common/util/NumberUtilsBenchmark.java
+++ src/test/java/com/thebuzzmedia/common/util/NumberUtilsBenchmark.java
@@ -1,3 +1,18 @@
+/**   
+ * Copyright 2011 The Buzz Media, LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.thebuzzmedia.common.util;
 
 import java.io.BufferedReader;
@@ -83,7 +98,7 @@ public class NumberUtilsBenchmark {
 				NumberUtils.parseInt(i, 5, DATA);
 			}
 		}
-		System.out.println("common-lib Integer.parseInt \telapsed time: "
+		System.out.println("common-lib NumberUtils.parseInt elapsed time: "
 				+ (System.currentTimeMillis() - time) + "ms\t("
 				+ (DATA_STRINGS.length * ITERS) + " numbers parsed, "
 				+ (length * ITERS) + " bytes processed)");
