e4d00179e2e4c4ab0aad15ea56a5a8d7174c8bfc
Riyad Kalla <software@thebuzzmedia.com>
2011-05-21 15:44:14 -0700

    Added new IService implmentation and test case.
    Updated README. Prepped for 2.2 release when ready.

---
+++ src/main/java/com/thebuzzmedia/common/concurrent/DefaultService.java
@@ -0,0 +1,62 @@
+/**   
+ * Copyright 2011 The Buzz Media, LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.thebuzzmedia.common.concurrent;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * Default implementation of {@link IService} utilizing a single, fixed-size
+ * {@link ThreadPoolExecutor}.
+ * 
+ * @author Riyad Kalla (software@thebuzzmedia.com)
+ */
+public class DefaultService implements IService {
+	public static final int DEFAULT_THREAD_POOL_SIZE = 64;
+
+	protected ExecutorService executor;
+
+	public DefaultService() {
+		this(DEFAULT_THREAD_POOL_SIZE);
+	}
+
+	public DefaultService(int threadPoolSize) throws IllegalArgumentException {
+		if (threadPoolSize < 1)
+			throw new IllegalArgumentException("threadPoolSize must be >= 1");
+
+		executor = Executors.newFixedThreadPool(threadPoolSize);
+	}
+
+	/**
+	 * Implemented to do a graceful shutdown using
+	 * {@link ThreadPoolExecutor#shutdown()} on the internal executor. All
+	 * pending tasks will be given a chance to complete.
+	 */
+	public void shutdown() {
+		executor.shutdown();
+	}
+
+	public ExecutorService getExecutorService() {
+		return executor;
+	}
+
+	public <T> Future<T> submit(Callable<T> task) {
+		return executor.submit(task);
+	}
+}
---
+++ src/main/java/com/thebuzzmedia/common/concurrent/IService.java
@@ -0,0 +1,75 @@
+/**   
+ * Copyright 2011 The Buzz Media, LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.thebuzzmedia.common.concurrent;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+/**
+ * Interface used to define a simple, {@link ExecutorService}-backed service
+ * used to process {@link Callable} tasks in parallel; returning an appropriate
+ * {@link Future} to the caller that will eventually contain the result of the
+ * operation (or <code>null</code> if there is no result).
+ * <p/>
+ * Services are meant as a natural compliment to {@link AbstractRetryableTask}s
+ * to provide an easy-to-use foundation for executing retryable tasks in a
+ * system.
+ * 
+ * @author Riyad Kalla (software@thebuzzmedia.com)
+ */
+public interface IService {
+	/**
+	 * Used to execute clean-up code necessary to "shutdown" this service.
+	 * Implementation details will depend on what resources a {@link IService}
+	 * holds on to and what needs to be cleaned up before exiting.
+	 */
+	public void shutdown();
+
+	/**
+	 * Used to retrieve the {@link ExecutorService} instance backing this
+	 * service.
+	 * <p/>
+	 * For shutting down a service an any backing resources like this one, do
+	 * not try and shutdown the {@link ExecutorService} directly, but instead
+	 * call {@link #shutdown()} to clean up any resources this service uses.
+	 * 
+	 * @return the {@link ExecutorService} instance backing this service.
+	 */
+	public ExecutorService getExecutorService();
+
+	/**
+	 * Used to execute the given {@link Callable} asynchronously using the
+	 * backing {@link #getExecutorService()} and returning a {@link Future}
+	 * representing the eventual result.
+	 * <p/>
+	 * Calling {@link Future#get()} is the easiest way to block on the task
+	 * until it returns a value or throws an exception indicating it failed.
+	 * 
+	 * @param <T>
+	 *            The type of the result returned by the {@link Future} when the
+	 *            result is ready.
+	 * @param task
+	 *            The task that will be executed asynchronously.
+	 * 
+	 * @return a {@link Future} representing the eventual result. Calling
+	 *         {@link Future#get()} is the easiest way for the calling thread to
+	 *         block on the asynchronous call until the result is either ready
+	 *         (and returned) or the task throws an exception indicating it
+	 *         failed.
+	 */
+	public <T> Future<T> submit(Callable<T> task);
+}
